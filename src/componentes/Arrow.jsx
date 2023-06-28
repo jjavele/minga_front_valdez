66bfac1a0ba65a8f5a71444066cc666f852dde18
valdeveloper <jjavaldz@gmail.com>
2023-06-28 06:32:43 -0400

    functional carousel

--- src/componentes/Arrow.jsx
+++ src/componentes/Arrow.jsx
@@ -1,5 +1,5 @@
-export default function Arrow({icon}) {
+export default function Arrow({icon, onClick}) {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 hidden md:block"><path strokeLinecap="round" strokeLinejoin="round" d={icon} /></svg>
+            <svg  onClick={onClick} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 hidden md:block"><path strokeLinecap="round" strokeLinejoin="round" d={icon} /></svg>
     )
 }
