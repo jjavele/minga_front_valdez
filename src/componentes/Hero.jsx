96c1ead3894239f82b5448c34fc4547910620ca4
valdeveloper <jjavaldz@gmail.com>
2023-06-28 04:23:28 -0400

    navbar desplegable

--- src/componentes/Hero.jsx
+++ src/componentes/Hero.jsx
@@ -2,7 +2,7 @@ import Button from "./button"
 
 export default function Hero() {
   return (
-    <div className="flex flex-col w-full h-[90vh] md:h-[40vh] justify-center items-center ">
+    <div className="flex flex-col w-full h-[90vh] md:h-[40vh] justify-center items-center">
         <h1 className="flex w-full items-center justify-center text-center text-[40px] md:text-[48px] text-[#FFF] [font-family:Roboto] font-bold">Your favorite comic bookstore✨</h1>
         <p className="w-[90vw] md:w-[70vw] items-center justify-center text-center text-[16px] md:text-[24px] text-[#FFF] [font-family:Roboto] pb-3">From classics to novelties, we have everything you need to immerse yourself in your favorite universes. Explore our catalog and live the adventure of your life.</p>
         <Button title = "Started" href = '/begin' />
